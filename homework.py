63a362a9205a81f9b5bafc1ad7710258a446950e
Treymf08 <treymf08@yandex.ru>
2021-06-23 21:45:03 +0300

    исправление после ревью

--- homework.py
+++ homework.py
@@ -17,67 +17,62 @@ class Calculator(Record):
     def __init__(self, limit):
         self.limit = limit
         self.records = []
+        self.today = dt.date.today()
 
     def add_record(self, record):
         self.records.append(record)
 
     def get_today_stats(self):
         today_stats = 0
-        for record in self.records:
-            if record.date == dt.date.today():
-                today_stats += record.amount
+        today_stats = sum(record.amount for record in self.records
+                          if record.date == self.today)
         return today_stats
 
     def get_week_stats(self):
-        self.week = dt.date.today() - dt.timedelta(days=7)
-        week_stats = sum(i.amount for i in self.records
-                         if i.date > self.week and i.date <= dt.date.today())
+        self.week = self.today - dt.timedelta(days=7)
+        week_stats = sum(record.amount for record in self.records
+                         if self.week <= record.date <= self.today)
         return week_stats
 
 
 class CashCalculator(Calculator):
-    '''Посчет денег.'''
+    """Посчет денег."""
 
-    def __init__(self, limit):
-        super().__init__(limit)
-    USD_RATE: float = 60.0
-    EURO_RATE: float = 70.0
-    RUB_RATE: float = 1
+    USD_RATE = 60.0
+    EURO_RATE = 70.0
+    RUB_RATE = 1
 
     def get_today_cash_remained(self, currency='rud'):
         dic_currency = {'rub': [1, 'руб'],
                         'eur': [self.EURO_RATE, 'Euro'],
                         'usd': [self.USD_RATE, 'USD']}
-        self.currency = currency
-        cash = self.limit - self.get_today_stats()
 
-        if self.currency == 'rub':
-            cash_currency = round(cash, 2)
-        elif self.currency == 'eur':
-            cash_currency = round(cash / dic_currency[currency][0], 2)
-        elif self.currency == 'usd':
-            cash_currency = round(cash / dic_currency[currency][0], 2)
+        if currency not in dic_currency:
+            raise ValueError(f'Валюта "{currency}" не известна')
+
+        today_stats = self.get_today_stats()
+
+        cash = self.limit - today_stats
+
+        cash_currency = round(cash / dic_currency[currency][0], 2)
 
         abs_cash = abs(cash_currency)
 
-        if self.get_today_stats() < self.limit:
+        if today_stats < self.limit:
             return (f'На сегодня осталось {cash_currency}'
-                    + f' {dic_currency[currency][1]}')
-        elif self.get_today_stats() == self.limit:
-            return('Денег нет, держись')
-        elif self.get_today_stats() > self.limit:
-            return (f'Денег нет, держись: твой долг - {abs_cash}'
-                    + f' {dic_currency[currency][1]}')
+                    f' {dic_currency[currency][1]}')
+        elif today_stats == self.limit:
+            return 'Денег нет, держись'
+        return (f'Денег нет, держись: твой долг - {abs_cash}'
+                f' {dic_currency[currency][1]}')
 
 
 class CaloriesCalculator(Calculator):
-    def __init__(self, limit):
-        super().__init__(limit)
+    """Подсчет каллорий."""
 
     def get_calories_remained(self):
         kcal = self.limit - self.get_today_stats()
-        if self.get_today_stats() >= self.limit:
-            return ('Хватит есть!')
-        else:
-            return('Сегодня можно съесть что-нибудь ещё,'
-                   + f' но с общей калорийностью не более {kcal} кКал')
+        if kcal <= 0:
+            return 'Хватит есть!'
+        return ('Сегодня можно съесть что-нибудь ещё,'
+                f' но с общей калорийностью не более {kcal} кКал')
